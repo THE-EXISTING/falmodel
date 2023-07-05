63471f3204f9e2f22fb85c262d0eaea40f0ade00
Nonthawit Doungsodsri <nonthawit.kub@gmail.com>
2023-07-05 10:49:27 +0700

    [+] update

--- lib/failures/failure.dart
+++ lib/failures/failure.dart
@@ -1,19 +1,27 @@
 // ignore_for_file: constant_identifier_names
 import 'package:falmodel/lib.dart';
 
-abstract class Failure extends Equatable {
+class Failure extends Equatable {
   final String? code;
   final String? message;
   final String? developMessage;
   final Exception? exception;
+  final StackTrace? stacktrace;
 
   const Failure({
     this.code,
     this.message,
     this.developMessage,
     this.exception,
+    this.stacktrace,
   });
 
   @override
-  List<Object?> get props => [code, message, developMessage, exception];
+  List<Object?> get props => [
+        code,
+        message,
+        developMessage,
+        exception,
+        stacktrace,
+      ];
 }
